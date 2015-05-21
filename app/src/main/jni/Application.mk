4d888ae708c327d9a011ee532eefc3dd7338e321
uberspot <onexemailx@gmail.com>
2015-05-21 15:52:56 +0300

    gradle now calls ndk-build as well

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := gnustl_static
 #APP_STL := stlport_static
 APP_PLATFORM := android-21
-APP_ABI := armeabi armeabi-v7a x86 #al
+APP_ABI := armeabi armeabi-v7a x86 #all
 APP_CFLAGS := -g -Wall -O1
 
 NDK_TOOLCHAIN_VERSION := 4.9
