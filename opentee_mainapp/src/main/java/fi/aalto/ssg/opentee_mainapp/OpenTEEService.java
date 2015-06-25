b73010462f9c1deceb84ffc0ad20f51c4ff8a492
uberspot <onexemailx@gmail.com>
2015-06-25 17:24:30 +0300

    clean up a bit and create method for executing binaries as well

--- opentee_mainapp/src/main/java/fi/aalto/ssg/opentee_mainapp/OpenTEEService.java
+++ opentee_mainapp/src/main/java/fi/aalto/ssg/opentee_mainapp/OpenTEEService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -19,6 +20,13 @@ import java.util.Arrays;
 public class OpenTEEService extends Service {
 
     public static final String OPEN_TEE_SERVICE_TAG = "OpenTEEService";
+
+    public static final String OPENTEE_ENGINE_BIN_NAME = "opentee-engine";
+
+    // Fields used for passing around data in messages
+    private static final int MSG_INSTALL_BIN = 1;
+    private static final int MSG_RUN_BIN = 2;
+    private static final String MSG_BINARY_NAME = "MSG_BINARY_NAME";
 
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
@@ -36,12 +44,23 @@ public class OpenTEEService extends Service {
         public void handleMessage(Message msg) {
             // Normally we would do some work here, like download a file.
             // For our sample, we just sleep for 5 seconds.
-            Log.i(OPEN_TEE_SERVICE_TAG, "CPU ABI " + Arrays.toString(Build.SUPPORTED_ABIS) + " " + System.getProperty("os.arch") );
-            installBinaryToHomeDir(mContext.get(), "opentee-engine");
+            Log.i(OPEN_TEE_SERVICE_TAG, "CPU ABI " + Arrays.toString(Build.SUPPORTED_ABIS) );
 
             // Stop the service using the startId, so that we don't stop
             // the service in the middle of handling another job
-            stopSelf(msg.arg1);
+            //stopSelf(msg.arg1);
+            Bundle data = msg.getData();
+            switch (msg.what) {
+                case OpenTEEService.MSG_INSTALL_BIN:
+                    installBinaryToHomeDir(mContext.get(), data.getString(MSG_BINARY_NAME));
+                    break;
+                case OpenTEEService.MSG_RUN_BIN:
+                    execBinaryFromHomeDir(mContext.get(), data.getString(MSG_BINARY_NAME));
+                    break;
+                // TODO Check if binary is already installed
+                default:
+                    super.handleMessage(msg);
+            }
         }
     }
 
@@ -64,8 +83,11 @@ public class OpenTEEService extends Service {
 
         // For each start request, send a message to start a job and deliver the
         // start ID so we know which request we're stopping when we finish the job
-        Message msg = mServiceHandler.obtainMessage();
+        Message msg = mServiceHandler.obtainMessage(MSG_INSTALL_BIN);
         msg.arg1 = startId;
+        Bundle b = new Bundle();
+        b.putString(MSG_BINARY_NAME, OPENTEE_ENGINE_BIN_NAME);
+        msg.setData(b);
         mServiceHandler.sendMessage(msg);
 
         // If we get killed, after returning from here, restart
@@ -83,30 +105,35 @@ public class OpenTEEService extends Service {
         Log.i(OPEN_TEE_SERVICE_TAG, "Service stopped");
     }
 
-    private void output(final String str) {
-        Runnable proc = new Runnable() {
-            public void run() {
-                Log.i(OPEN_TEE_SERVICE_TAG, str);
-            }
-        };
-        mServiceHandler.post(proc);
-    }
-
     private void installBinaryToHomeDir(final Context context, final String binaryName) {
-        final String destPath = context.getApplicationInfo().dataDir + File.separator + binaryName;
-        final String originAssetPath = Build.SUPPORTED_ABIS[0] + File.separator + binaryName;
-        Log.i(OPEN_TEE_SERVICE_TAG, "Home Dir: " + destPath);
-
         Thread thread = new Thread(new Runnable() {
             public void run() {
                 try {
                     String output = "";
-                    Log.i(OPEN_TEE_SERVICE_TAG, "Copying " + originAssetPath + " TO " + destPath);
+                    String destPath = context.getApplicationInfo().dataDir + File.separator + binaryName;
+                    String originAssetPath = Build.SUPPORTED_ABIS[0] + File.separator + binaryName;
+                    Log.d(OPEN_TEE_SERVICE_TAG, "Home Dir: " + destPath);
+
+                    Log.d(OPEN_TEE_SERVICE_TAG, "Copying " + originAssetPath + " TO " + destPath);
                     Utils.copyFromAssetsToAppDir(context, originAssetPath, destPath);
                     output = Utils.execUnixCommand("/system/bin/chmod 744 " + destPath);
-                    Log.i(OPEN_TEE_SERVICE_TAG, "Chmod returned: " + output);
-                    //output = Utils.execUnixCommand(destPath);
-                    //Log.i(OPEN_TEE_SERVICE_TAG, "Execution of binary returned: " + output);
+                    Log.d(OPEN_TEE_SERVICE_TAG, "Chmod returned: " + output);
+                } catch (InterruptedException | IOException e) {
+                    Log.e(OPEN_TEE_SERVICE_TAG, e.getMessage());
+                }
+            }
+        });
+        thread.start();
+    }
+
+    private void execBinaryFromHomeDir(final Context context, final String binaryName) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    String output = "";
+                    String destPath = context.getApplicationInfo().dataDir + File.separator + binaryName;
+                    output = Utils.execUnixCommand(destPath);
+                    Log.d(OPEN_TEE_SERVICE_TAG, "Execution of binary returned: " + output);
                 } catch (InterruptedException | IOException e) {
                     Log.e(OPEN_TEE_SERVICE_TAG, e.getMessage());
                 }
