ce0adc3394468b3b49deb7aa6175534b89218056
uberspot <onexemailx@gmail.com>
2015-08-19 18:47:55 +0300

    remove the openteelib module since it's irrelevant with the service

--- openteelib/src/main/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_STL := gnustl_static
-APP_PLATFORM := android-21
-APP_ABI := armeabi armeabi-v7a x86 #all
-APP_CFLAGS := -g -Wall -O2
-
-APP_BUILD_SCRIPT := Android.mk
-NDK_TOOLCHAIN_VERSION := 4.9
